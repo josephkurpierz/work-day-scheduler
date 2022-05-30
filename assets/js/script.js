6e0a8b68adb3199d83702a483f061d723dc4a0ee
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-29 20:50:11 -0500

    added first row and some pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// set date in header using momentjs for current day format moment().format("dddd,MMMM Do")
+//$("#currentDay").moment().format("dddd, MMMM Do");
+// 
+
+//create timeblocks from 9AM-5PM moment().format("hA")
+
+
+// add colors, current hour = bg-danger, not happened yet = bg-success, old =bg-secondary
+// moment object evaluating against time
+// text is always dark...last class
+//like taskMaster, check status every 15min to evaluate current time
+
+
+// edit middle column with jquery textarea
+
+//save with right column
+//localStorage.setItem("task", middlecolumn)
+
+
