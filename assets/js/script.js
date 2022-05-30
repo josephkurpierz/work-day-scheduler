b2e848d727f27cec791f42bdb1cd215491498c93
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-30 00:12:38 -0500

    added date to top and dynamically created table that doesn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+var tasks = {};
+
+//create task bars
+var createTaskBar = function () {
+  var timeBlock = $("<div>").addClass("row");
+  var taskHour = $("<div>").addClass("col hour");
+  var taskText = $("<textarea>").addClass("col-9");
+  var taskSave = $("<div>").addClass("col saveBtn");
+  timeBlock.append(taskHour, taskText, taskSave);
+  $(".container").append(timeBlock);
+};
+
+var initializePage = function() {
+  var workHours = 8;
+  for (var i=0;i<workHours;i++){
+    createTaskBar();
+  }
+};
+
 // set date in header using momentjs for current day format moment().format("dddd,MMMM Do")
-//$("#currentDay").moment().format("dddd, MMMM Do");
-// 
+$("#currentDay").append(moment().format("dddd, MMMM Do"));
 
 //create timeblocks from 9AM-5PM moment().format("hA")
-
+var hourBlock = $("#hour");
 
 // add colors, current hour = bg-danger, not happened yet = bg-success, old =bg-secondary
 // moment object evaluating against time
@@ -16,4 +34,4 @@
 //save with right column
 //localStorage.setItem("task", middlecolumn)
 
-
+initializePage();
