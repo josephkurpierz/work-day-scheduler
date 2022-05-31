bc50c11541f060399c4c21493d0f51ad415a2cb2
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-31 17:52:31 -0500

    added some pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,26 +21,24 @@ var stuffToSave = {
 // };
 // setTime();
 
-  // added task bars in HTML
   // create task bars
 var createTaskBar = function (index) {
   //create the row
   var timeBlock = $("<div>").addClass("row");
   //create the columns
-  var taskHour = $("<div>").addClass("col hour").attr("data", index);
+  var taskHour = $("<div>").addClass("col hour").attr("data-index", index);
   taskHour.value = moment().set("hour", 9+index).format("hA");
   console.log(taskHour.value);
-  var taskText = $("<textarea>").addClass("col-9").attr("data", index);
-  var taskSave = $("<div>").addClass("col saveBtn").attr("data", index);
+  var taskText = $("<textarea>").addClass("col-9").attr("data-index", index);
+  var taskSave = $("<div>").addClass("col saveBtn").attr("data-index", index);
   // create the icon
-  var saveIcon = $("<i>").addClass("fa-solid fa-lock").attr("data", index);
+  var saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk").attr("data-index", index);
   // attach the icon to the save column
   taskSave.append(saveIcon);
   //attach all three columns to the row
   timeBlock.append(taskHour, taskText, taskSave);
   //add the row containing 3 columns to the page
   $(".container").append(timeBlock);
-
 };
 
 var initializePage = function () {
@@ -51,41 +49,34 @@ var initializePage = function () {
   //var taskText = $("<textarea>").addClass("col-9");
   //var taskSave = $("<div>").addClass("col saveBtn");
   // create the icon
-  //var saveIcon = $("<i>").addClass("fa-solid fa-lock");
+  //var saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk");
   for (var i = 0; i < workHours; i++) {
-    //var newHour = $("taskHour").addId("index"+i);
     createTaskBar(i);
-    //$(document.querySelector(".hour").data)
+    //assign timeblocks time(left column) from 9AM-5PM with moment().format("hA")
     //$("newHour").value = moment().set("hour", 9 + i).format("hA");
     //target first column of row and add time to it as it is created
     //maybe I need to do it in HTML?? don't know how to target dynamic creations
   }
 };
+
 initializePage();
-// // Construct some new DOM element.
-// $(whatever).html('... id="mynewthing"...');
-
-// // This won't work...
-// $("#mynewthing")...
-
-// // But this will...
-// $(document.getElementById("mynewthing"))...
-
-
-
-//create timeblocks from 9AM-5PM moment().format("hA")
-
-
 
 // add colors, current hour = bg-danger, not happened yet = bg-success, old =bg-secondary
 // moment object evaluating against time
 // text is always dark...last class
-//like taskMaster, check status every 15min to evaluate current time
+// set interval (1 hour) and check timeblock time vs moment().get("hour")
+// if timeblock time < get(hour) setClass("past")
+// if timeblock = get(hour) setClass("present")
+// if timeblock > get(hour) setClass("future")
+// if current moment() is > current day, clear localStorage
 
 
 // edit middle column with jquery textarea
-
-//save with right column
-//localStorage.setItem("task", middlecolumn)
+// should be native with textarea?
+// save with right column
+// on("click", function(){
+  //save middle column to array with index
+// })
+// localStorage.setItem("task", middlecolumn)
 
 //initializePage();
